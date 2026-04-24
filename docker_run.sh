550dde38c5988b97258b4ffca31f81635e2506cb
Shuheng Zheng <johnz622@gmail.com>
2026-04-24 10:52:21 -0400

    docker_run.sh: no need to set container name

--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Use the current directory name as the container name
-CONTAINER_NAME="ray_dev_$(basename "$PWD")"
-
-docker run -it --rm \
-  --name "$CONTAINER_NAME" \
+docker run -it \
   --gpus all \
   --shm-size=2gb \
   -v "$(pwd):/home/ray/project" \
   -w /home/ray/project \
   -p 8265:8265 \
-  rayproject/ray-ml:latest-py310-gpu \
+  minesweeper-ai:latest \
   bash
