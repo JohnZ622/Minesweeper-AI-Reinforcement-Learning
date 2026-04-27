0846abea0f353fae25b20d73a0444671b88d92dc
Shuheng Zheng <johnz622@gmail.com>
2026-04-27 06:47:14 -0400

    docker_run.sh: publish port 6006 to host to enable Tensorboard

--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker run -it \
+docker run -it --rm \
   --gpus all \
   --shm-size=2gb \
   -v "$(pwd):/home/ray/project" \
   -w /home/ray/project \
   -p 8265:8265 \
+  -p 6006:6006 \
   minesweeper-ai:latest \
   bash
